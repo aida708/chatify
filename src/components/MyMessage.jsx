492a583f7415ee52c51f50de99d2f6eb819cb0e4
Aida <aidasufaj00@gmail.com>
2023-12-12 17:58:11 +0100

    working on rendering MyMessage && images

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
-function MyMessage() {
-  return <div></div>;
+function MyMessage({ message }) {
+  if (message.attachements && message.attachements.length > 0) {
+    // costumized html tag called props:
+    return (
+      <img
+        src={message.attachements[0].file}
+        alt="message-attach"
+        className="message-image"
+        style={{ float: "right" }}
+      />
+    );
+  }
+  return (
+    <div
+      className="message"
+      style={{
+        float: "right",
+        marginRight: "18px",
+        color: "white",
+        backgroundColor: "#3B2A50",
+      }}
+    >
+      {message.text}
+    </div>
+  );
 }
 
 export default MyMessage;
